eaf48ef911b15c8120a2874615e045fbccf1664a
Sjef van Leeuwen <36629650+sjefvanleeuwen@users.noreply.github.com>
2018-04-12 14:03:48 +0000

    Add project files.

---
+++ NeoGovern.NeoGovernDid/DidDocument.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace NeoGovern.NeoGovernDid
+{
+    public class DidDocument
+    {
+        [JsonProperty("@context")]
+        public string Context => "https://w3id.org/did/v1";
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("publicKey")]
+        public IEnumerable<PublicKey> PublicKey { get; set; }
+        [JsonProperty("service")]
+        public IEnumerable<Service> Service { get; set; }
+        [JsonProperty("authentication")]
+        public IEnumerable<Authentication> Authentication { get; set; }
+        
+    }
+
+    public class PublicKey
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("owner")]
+        public string Owner { get; set; }
+        [JsonProperty("publicKeyPem")]
+        public string PublicKeyPem { get; set; }
+    }
+
+    public class Authentication {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("publicKey")]
+        public string PublicKey { get; set; }
+    }
+
+    public class Service
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("serviceEndpoint")]
+        public string ServiceEndpoint { get; set; }
+    }
+}
---
+++ NeoGovernNeogovernDidTests/DidDocumentTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoGovern.NeoGovernDid;
+using Newtonsoft.Json;
+
+namespace NeoGovernNeogovernDidTests
+{
+    [TestClass]
+    public class DidDocumentTests
+    {
+        /// <summary>
+        /// 1.3.1 Self-Managed DID Document
+        /// </summary>
+        [TestMethod]
+        public void ShouldCreateMinimalSelfManagedDidDocument()
+        {
+            var doc = new DidDocument
+            {
+                Id = "did:example:123456789abcdefghi",
+                PublicKey = new List<PublicKey>()
+                {
+                    new PublicKey()
+                    {
+                        Id = "did:example:123456789abcdefghi#keys-1",
+                        Type = "RsaVerificationKey2018",
+                        Owner = "did:example:123456789abcdefghi",
+                        PublicKeyPem = "-----BEGIN PUBLIC KEY...END PUBLIC KEY-----\r\n"
+                    }
+                },
+                Authentication = new List<Authentication>()
+                {
+                    new Authentication()
+                    {
+                        Type = "RsaSignatureAuthentication2018",
+                        PublicKey = "did:example:123456789abcdefghi#keys-1"
+                    }
+                },
+                Service = new List<Service>()
+                {
+                    new Service()
+                    {
+                        Type="ExampleService",
+                        ServiceEndpoint = "https://example.com/endpoint/8377464"
+                    }
+                }
+            };
+
+            var result = JsonConvert.SerializeObject(doc);
+            Assert.IsTrue(result== "{\"@context\":\"https://w3id.org/did/v1\",\"id\":\"did:example:123456789abcdefghi\",\"publicKey\":[{\"id\":\"did:example:123456789abcdefghi#keys-1\",\"type\":\"RsaVerificationKey2018\",\"owner\":\"did:example:123456789abcdefghi\",\"publicKeyPem\":\"-----BEGIN PUBLIC KEY...END PUBLIC KEY-----\\r\\n\"}],\"service\":[{\"type\":\"ExampleService\",\"serviceEndpoint\":\"https://example.com/endpoint/8377464\"}],\"authentication\":[{\"type\":\"RsaSignatureAuthentication2018\",\"publicKey\":\"did:example:123456789abcdefghi#keys-1\"}]}");
+        }
+    }
+}
