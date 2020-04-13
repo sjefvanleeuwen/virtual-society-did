cf75ab60904dfa5a2c49c3077b5f778d978d70f4
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-13 22:42:41 +0200

    Adding default controller for Swagger Redirect

---
+++ VirtualSociety.VirtualSocietyDidApi/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VirtualSociety.VirtualSocietyDidApi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Route(""), HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public RedirectResult RedirectToSwaggerUi()
+        {
+            return Redirect("/swagger/");
+        }
+    }
+}
