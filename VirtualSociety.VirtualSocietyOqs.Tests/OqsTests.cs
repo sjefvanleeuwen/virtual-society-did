8a2d031e1beb0184c6b26642098afcf850ff8869
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-13 13:50:00 +0200

    Quantum infrastructure Can Discover All Key Encapsulation Mechanisms

---
+++ VirtualSociety.VirtualSocietyOqs.Tests/OqsTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VirtualSociety.VirtualSocietyOqs.Tests
+{
+    public class OqsTests
+    {
+        [Fact]
+        public void CanDiscoverAllKeyEncapsulationMechanisms()
+        {
+            Assert.True(KEM.SupportedMechanisms.Count == 61);
+            
+        }
+    }
+}
