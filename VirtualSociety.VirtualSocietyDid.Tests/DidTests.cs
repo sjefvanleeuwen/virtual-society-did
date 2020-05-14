49b94e059eae4d493bbf3f15f4007adb77d15718
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-14 12:07:33 +0200

    Write did URI Extensions and Tests

---
+++ VirtualSociety.VirtualSocietyDid.Tests/DidTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace VirtualSociety.VirtualSocietyDid.Tests
+{
+    public class DidTests
+    {
+        [Fact]
+        public async void CanExtractDidUriMethodElements()
+        {
+            var url = new Uri("did:vsoc:gen:actor:21tDAKCERh95uGgKbJNHYp");
+            var did = url.Did();
+            Assert.Equal(did.Scheme, "did");
+            Assert.Equal(did.Method, "vsoc");
+            Assert.Equal(did.MethodNameSpace, "gen:actor");
+            Assert.Equal(did.MethodIdentifier, "21tDAKCERh95uGgKbJNHYp");
+        }
+
+        [Fact]
+        public async void CanCreateUniqueIdentifier()
+        {
+            // multiple passes, make sure no invalid character result from guid => base64 conversion.
+            for (int i = 0; i < 100; i++)
+            {
+                var result = Did.GenerateMethodIdentifier();
+                Assert.True(result.All(c => char.IsLetterOrDigit(c)));
+            }
+        }
+
+        [Fact]
+        public async void CanCreateDidWithNameSpace()
+        {
+            var did = new Did("gen:actor");
+            Assert.Equal($"did:vsoc:gen:actor:{did.MethodIdentifier}", did.ToString());
+        }
+
+        [Fact]
+        public async void CanCreateDidWithoutNameSpace()
+        {
+            var did = new Did();
+            Assert.Equal($"did:vsoc:{did.MethodIdentifier}", did.ToString());
+        }
+
+        [Fact]
+        public async void ShouldbeAllLowerCaseExceptMethodIdentifier()
+        {
+            var did = new Did("gEn:acTor","ExAMPle");
+            Assert.Equal($"did:example:gen:actor:{did.MethodIdentifier}", did.ToString());
+        }
+    }
+}
---
+++ VirtualSociety.VirtualSocietyDid/Did.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace VirtualSociety.VirtualSocietyDid
+{
+    /// </summary>
+    public static class DidUriExtension
+    {
+        /// <summary>
+        /// Extends the Microsoft .NET Uri specification with the did url specification format.
+        /// NOTE: (partially implemented).
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <returns></returns>
+        public static Did Did(this Uri uri)
+        {
+            var did = new Did();
+            var segments = uri.AbsolutePath.Split(':');
+            did.Method = segments[0];
+            did.MethodNameSpace = string.Join(':', segments.Skip(1).Take(segments.Length - 2));
+            did.MethodIdentifier = segments.Last();
+            return did;
+        }
+    }
+
+    public class Did
+    {
+        public string Scheme => "did";
+        public string Method {get;set;}
+        public string MethodIdentifier { get; set; }
+        public string MethodNameSpace { get; set; }
+
+        /// <summary>Generates the method identifier.</summary>
+        /// <returns>base64 guid</returns>
+        public static string GenerateMethodIdentifier()
+        {
+            return Regex.Replace(Convert.ToBase64String(Guid.NewGuid().ToByteArray()), "[/+=]", "");
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Did"/> class.
+        /// </summary>
+        public Did()
+        {
+            Method = "vsoc";
+            MethodNameSpace = string.Empty;
+            MethodIdentifier = GenerateMethodIdentifier();
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Did"/> class.</summary>
+        /// <param name="methodNameSpace"></param>
+        /// <param name="method">The method (defaults to vsoc).</param>
+        public Did(string methodNameSpace, string method = "vsoc")
+        {
+            Method = method.ToLower();
+            MethodNameSpace = methodNameSpace.ToLower();
+            MethodIdentifier = GenerateMethodIdentifier();
+        }
+
+        public string ToString()
+        {
+            return string.Join(":",
+                (new []{Scheme,Method,MethodNameSpace,MethodIdentifier})
+                .Where(s => !string.IsNullOrEmpty(s)));
+        }
+    }
+}
