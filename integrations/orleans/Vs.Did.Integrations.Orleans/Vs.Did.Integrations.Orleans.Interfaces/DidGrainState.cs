8d32a72d64f8f58048895fc8fa357f628c869bea
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-15 16:09:21 +0200

    Did Grain State (for future persistent grains)

---
+++ integrations/orleans/Vs.Did.Integrations.Orleans/Vs.Did.Integrations.Orleans.Interfaces/DidGrainState.cs
@@ -0,0 +1,9 @@
+﻿using Vs.Did.Abstractions.Interfaces;
+
+namespace Vs.Did.Integrations.Orleans
+{
+    public class DidGrainState
+    {
+        public IDid Did {get;set;}
+    }
+}
---
+++ integrations/orleans/Vs.Did.Integrations.Orleans/Vs.Did.Integrations.Orleans.Interfaces/IDidGrain.cs
@@ -0,0 +1,11 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+using Vs.Did.Abstractions.Interfaces;
+
+namespace Vs.Did.Integrations.Orleans.Interfaces
+{
+    public interface IDidGrain : IGrainWithGuidKey
+    {
+        Task<IDid> CreateDid(IMethodNamespace type);
+    }
+}
