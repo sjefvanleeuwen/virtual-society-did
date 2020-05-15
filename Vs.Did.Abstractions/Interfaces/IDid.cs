aa67181335aa9ecd39205f3b863ea4459991527f
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-15 16:07:03 +0200

    Speficying IDid Interface

---
+++ Vs.Did.Abstractions/Interfaces/IDid.cs
@@ -0,0 +1,12 @@
+﻿namespace Vs.Did.Abstractions.Interfaces
+{
+    public interface IDid
+    {
+        string Method { get; set; }
+        string MethodIdentifier { get; set; }
+        string MethodNameSpace { get; set; }
+        string Scheme { get; }
+
+        string ToString();
+    }
+}
