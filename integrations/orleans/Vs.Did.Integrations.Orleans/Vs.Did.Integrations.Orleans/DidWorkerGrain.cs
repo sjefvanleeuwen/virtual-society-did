58fad7cce1f7f37d0eae29a5995d3adf5d74321b
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-15 16:08:57 +0200

    DID reentrant stateless worker grain (no persistence yet)

---
+++ integrations/orleans/Vs.Did.Integrations.Orleans/Vs.Did.Integrations.Orleans/DidWorkerGrain.cs
@@ -0,0 +1,20 @@
+﻿using Orleans;
+using Orleans.Concurrency;
+using System.Threading.Tasks;
+using Vs.Did.Abstractions.Interfaces;
+using Vs.Did.Integrations.Orleans.Interfaces;
+
+namespace Vs.Did.Integrations.Orleans
+{
+    /// <summary></summary>
+    /// <seealso cref="Orleans.Grain" />
+    /// <seealso cref="Vs.Did.Integrations.Orleans.Interfaces.IDidGrain" />
+    [StatelessWorker] [Reentrant]
+    public class DidWorkerGrain : Grain, IDidGrain
+    {
+        public async Task<IDid> CreateDid(IMethodNamespace type)
+        {
+            return new Did(type.Namespace);
+        }
+    }
+}
